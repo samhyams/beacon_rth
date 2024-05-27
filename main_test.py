df3a679e2a3af7f49e463420cdaf166f7489d4dd
Sam Hyams <s.hyams.com@gmail.com>
2024-05-27 17:00:23 +0100

    Refactored with a dedicated new_wp function, now works for multiple sequential waypoints

--- main_test.py
+++ main_test.py
@@ -5,9 +5,50 @@
 from pymavlink_plane import *
 from clrprint import *
 
+#### GLOBALS ####
+# Constant altitude for all commands
+ALT = 100
+# Waypoints to be used
+WPS = [
+    (51.12067178, -2.18868379),
+    (51.12567178, -2.18868379),
+    (51.12567178, -2.18468379)
+]
+# Counter to see how many waypoints we have commanded
+WP_COUNTER = 0
+
+def new_wp(plane, location, accuracy):
+    '''
+        Creates new flightplan with just home and defined waypoint
+        Uploads to mav
+        Inputs:
+        - plane = Plane object
+        - location = (lat, lon) tuple
+        - accuracy = int of closest approach to WP [m]
+    '''
+    clrprint('Adding WP at ', location, clr='y')
+    # CLear existing (keep home)
+    plane.init_wp()
+    # Add the WP
+    plane.add_waypoint(location[0], location[1], ALT)
+    global WP_COUNTER
+    WP_COUNTER += 1
+    # Upload the plan
+    plane.send_all_waypoints()
+    # Set the flight mode
+    clrprint('Setting AUTO', clr='y')
+    plane.change_mode('AUTO')
+    clrprint('AUTO set :)', clr='g')
+    clrprint('Current WP# = ', plane.mav.waypoint_current(), ', Total WPs = ', WP_COUNTER, clr='b')
+    # Get the current target waypoint info: target.alt, target.lat, larget.lng
+    target = plane.get_current_target()
+    print(target)
+
+    # Wait until we approach the target (within <accuracy> m, wait 1000 sec max)
+    plane.wait_location(target, accuracy=accuracy, timeout=1000)
+    clrprint('At WP# = ', plane.mav.waypoint_current(), clr='g')
+
 def main():
-    # Constant altitude for all commands
-    ALT = 100
 
     # Initialise the object
     plane = Plane()
@@ -22,39 +63,17 @@ def main():
     # Get the home location info: home.alt, home.lat, home.lng
     home = plane.home_position_as_mav_location()
 
-    ## Create a new plan for upload
-    # Add a waypoint to the plan (west a bit)
-    plane.add_waypoint(51.12067178, -2.18868379, ALT)
-    # Upload the plan
-    plane.send_all_waypoints()
-    # Set the flight mode
-    clrprint('Setting AUTO', clr='y')
-    plane.change_mode('AUTO')
-    clrprint('AUTO set :)', clr='g')
-    clrprint('Current WP# = ', plane.mav.waypoint_current(), clr='b')
-    # Get the current target waypoint info: target.alt, target.lat, larget.lng
-    target = plane.get_current_target()
-    print(target)
+    # Add the first WP and send
+    new_wp(plane, WPS[0], 100)
 
-    # Wait until we approach the target (within 250 m, wait 1000 sec max)
-    plane.wait_location(target, accuracy=250, timeout=1000)
+    # Add the second WP and send
+    new_wp(plane, WPS[1], 100)
 
-    plane.wploader.clear()
-    # Add a waypoint to the plan (north a bit)
-    plane.add_waypoint(51.12567178, -2.18868379, ALT)
-    # Upload the plan
-    plane.send_all_waypoints()
-    # Set the flight mode
-    plane.change_mode('AUTO')
-    print('*****************************',plane.mav.waypoint_current())
-    
-    # Get the current target waypoint info: target.alt, target.lat, larget.lng
-    target = plane.get_current_target()
-    print(target)
+    # Add the third WP and send
+    new_wp(plane, WPS[2], 100)
 
-    # Wait until we approach the target (within 250 m, wait 1000 sec max)
-    plane.wait_location(target, accuracy=50, timeout=1000)
-
+    # No more commands
+    clrprint('Finished, actioning RTL default', clr='red')
 
 if __name__ == "__main__":
     main()
